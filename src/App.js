0e2c50475e795e5524c074257a1051dad73f8451
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-02-28 11:16:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Persons from './Components/Persons/Persons';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className='text-center text-primary py-3'> &rarr; Workplace &larr;</h2>
+      <div className="d-flex flex-wrap">
+        <Persons></Persons>
+      </div>
     </div>
   );
 }
